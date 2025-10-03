0b8c18725b9b8bf865174003fc8018899dd1dbb7
Kenjiro Fukuda <kenjirofukuda@gmail.com>
2025-10-03 21:09:35 +0900

    add header type

--- example1.php
+++ example1.php
@@ -1,5 +1,76 @@
 <?php
 
+$HEADER_MAP = [
+'HEADER'       => 0,
+'BGNLIB'       => 1,
+'LIBNAME'      => 2,
+'UNITS'        => 3,
+'ENDLIB'       => 4,
+'BGNSTR'       => 5,
+'STRNAME'      => 6,
+'ENDSTR'       => 7,
+'BOUNDARY'     => 8,
+'PATH'         => 9,
+'SREF'         => 10,
+'AREF'         => 11,
+'TEXT'         => 12,
+'LAYER'        => 13,
+'DATATYPE'     => 14,
+'WIDTH'        => 15,
+'XY'           => 16,
+'ENDEL'        => 17,
+'SNAME'        => 18,
+'COLROW'       => 19,
+'TEXTNODE'     => 20,
+'NODE'         => 21,
+'TEXTTYPE'     => 22,
+'PRESENTATION' => 23,
+'SPACING'      => 24,
+'STRING'       => 25,
+'STRANS'       => 26,
+'MAG'          => 27,
+'ANGLE'        => 28,
+'UINTEGER'     => 29,
+'USTRING'      => 30,
+'REFLIBS'      => 31,
+'FONTS'        => 32,
+'PATHTYPE'     => 33,
+'GENERATIONS'  => 34,
+'ATTRTABLE'    => 35,
+'STYPTABLE'    => 36,
+'STRTYPE'      => 37,
+'ELFLAGS'      => 38,
+'ELKEY'        => 39,
+'LINKTYPE'     => 40,
+'LINKKEYS'     => 41,
+'NODETYPE'     => 42,
+'PROPATTR'     => 43,
+'PROPVALUE'    => 44,
+'BOX'          => 45,
+'BOXTYPE'      => 46,
+'PLEX'         => 47,
+'BGNEXTN'      => 48,
+'ENDEXTN'      => 49,
+'TAPENUM'      => 50,
+'TAPECODE'     => 51,
+'STRCLASS'     => 52,
+'RESERVED'     => 53,
+'FORMAT'       => 54,
+'MASK'         => 55,
+'ENDMASKS'     => 56,
+];
+
+
+$HEADER_INVERT_MAP = [];
+foreach ($HEADER_MAP as $key => $value) {
+    $HEADER_INVERT_MAP[$value] = $key;
+}
+
+function header_symbol(int $value): string {
+    global $HEADER_INVERT_MAP;
+    return $HEADER_INVERT_MAP[$value];
+}
+
 $gdspath = join('/', [getenv('HOME'), 'Nextcloud', 'gds', 'GDSreader.0.3.2','test.gds']);
 if (! file_exists($gdspath)) {
     echo "Not found: $gdspath";
@@ -27,6 +98,19 @@ function next_bytearray($fh): array {
     return $bytes;
 }
 
+
+function handle_record(array $bytes): void {
+    global $HEADER_MAP;
+    $rec_type = $bytes[1];
+    $data_type = $bytes[2];
+    $data = array_slice($bytes, 2);
+    var_dump([header_symbol($rec_type), $data]); 
+    switch ($rec_type) {
+    case $HEADER_MAP['BGNLIB']:
+        break;
+    }
+}
+
 $rec_count = 0;
 try {
     $fh = fopen($gdspath, 'rb');
@@ -35,9 +119,7 @@ try {
         if (count($bytes) == 0) {
             break;
         }
-        $rec_type = $bytes[1];
-        $data_type = $bytes[2];
-        $data = array_slice($bytes, 2);
+        handle_record($bytes);
         $rec_count++;
     }
 }
@@ -49,4 +131,4 @@ finally {
 }
 echo $rec_count, "\n";
 
-?>
+var_dump($HEADER_MAP);
